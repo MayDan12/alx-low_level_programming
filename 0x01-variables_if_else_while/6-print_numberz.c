c245d4e5c8b73b5833275e6574e0ce163de5dc9d
MayDan12 <oluwamayowadan@gmail.com>
2023-07-15 02:16:35 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print digit of base 10
+ * Return: 0 always (excellent)
+ */
+int main(void)
+{
+int may;
+may = 0;
+while (may < 10)
+{
+putchar(may + '0');
+may++
+}
+putchar('\n');
+return (0);
+}
