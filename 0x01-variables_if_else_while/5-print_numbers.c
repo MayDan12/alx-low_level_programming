707c6b6a2d7b60e52e01525733e9d945ca5b4f77
MayDan12 <oluwamayowadan@gmail.com>
2023-07-15 02:11:01 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print all single digit numbers
+ * Return: 0 always (excellent)
+ */
+int main(void)
+{
+int dan;
+for (dan = 0; dan < 10; dan++)
+	printf("%d", dan);
+putchar('\n');
+return (0);
+}
