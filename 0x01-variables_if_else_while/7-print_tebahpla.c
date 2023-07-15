8778e19ebddde1d41d3e256b3ed232140a6dc511
MayDan12 <oluwamayowadan@gmail.com>
2023-07-15 02:25:25 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - reverse alphabet
+ * Return: 0 always (excellent)
+ */
+int main(void)
+{
+char MD;
+
+MD = 'z';
+while
+	(MD >= 'a')
+	{
+		putchar(MD);
+		MD++;
+	}
+putchar('\n');
+return (0);
+}
