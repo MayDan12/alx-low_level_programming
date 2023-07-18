22b16dd65165d3e578cad46702d10eeb2a265c2e
MayDan12 <oluwamayowadan@gmail.com>
2023-07-18 07:13:01 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - i love my alphabet
+ * written by dannyboy12
+ * Return 0 always (success)
+ */
+void print_alphabet(void)
+{
+	char alpha;
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar(alpha);
+	}
+	_putchar('\n');
+}
