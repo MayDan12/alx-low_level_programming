bc186d4f9c49d71b8ffd7f8d076ca60e579c57e7
MayDan12 <oluwamayowadan@gmail.com>
2023-07-20 07:06:53 -0700

    3-prints_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - this prints number fronm o to ni9ne
+ * written by oluwanimotele daniel
+ * Return: o always success
+ */
+
+void print_numbers(void)
+{
+	int dan;
+
+	for (dan = 48; dan < 58; dan++)
+	{
+		_putchar(dan);
+	}
+	_putchar('\n');
+}
