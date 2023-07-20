985b6b226d27a209acdef7a5d429bf69f18cf703
MayDan12 <oluwamayowadan@gmail.com>
2023-07-20 06:45:01 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - this return two digit of number
+ * @d: is a good boy
+ * @m: is bad boy
+ * written by daniel oluwanimotele
+ * Return: i love u always
+ */
+
+int mul(int d, int m)
+{
+	int c;
+
+	c = d * m;
+	return (c);
+}
