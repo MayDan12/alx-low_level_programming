5d798aabb6973539b4a8685f54b284cf749c13b5
MayDan12 <oluwamayowadan@gmail.com>
2023-07-26 05:31:14 -0700

    103-infinite_add.c

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,79 @@
+#include "main.h"
+#include "main.h"
+
+/**
+ * rev_string - reverse array
+ * @n: integer params
+ * Return: 0
+ */
+
+void rev_string(char *n)
+{
+	int d = 0;
+	int m = 0;
+	char temp;
+
+	while (*(n + d) != '\0')
+	{
+		d++;
+	}
+	d--;
+
+	for (m = 0; m < d; m++, d--)
+	{
+		temp = *(n + m);
+		*(n + m) = *(n + d);
+		*(n + d) = temp;
+	}
+}
+
+/**
+ * infinite_add - This function adds two integer or number
+ * written by oluwanimotele daniel
+ * @n1: input value
+ * @n2: input value
+ * @r: pointer to buffer
+ * @size_r: this is the buffer size
+ * Return: o always
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
+
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
+		return (0);
+	while (j >= 0 || i >= 0 || overflow == 1)
+	{
+		if (i < 0)
+			val1 = 0;
+		else
+			val1 = *(n1 + i) - '0';
+		if (j < 0)
+			val2 = 0;
+		else
+			val2 = *(n2 + j) - '0';
+		temp_tot = val1 + val2 + overflow;
+		if (temp_tot >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+		if (digits >= (size_r - 1))
+			return (0);
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
+		j--;
+		i--;
+	}
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
+	return (r);
+}
