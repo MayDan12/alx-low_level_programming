9113949a073094f06fc73ebcc092bc7aa94dffff
MayDan12 <oluwamayowadan@gmail.com>
2023-08-04 05:53:41 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - this prints the nanme of the program
+ * @argc: number of argument passed
+ * @argv: the array of argument
+ * written by daniel
+ * Return: o aiways
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+0x0A-argc_argv
+this is a projects on argument vector and argument count
