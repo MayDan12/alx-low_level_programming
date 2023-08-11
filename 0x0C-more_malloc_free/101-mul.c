58a6c39b5ba97a13ecfca4479163bbfe9448c8df
MayDan12 <oluwamayowadan@gmail.com>
2023-08-11 16:14:38 -0700

    rubbish

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * good_boy - is a good boy
+ * @d: input
+ * written by daniel oluwanimotele
+ * Return: 0 always
+ */
+int good_boy(char d)
+{
+	int x, y;
+	int z;
+
+	x = 7;
+	y = 20;
+
+	z = x * y;
+	d = z;
+	_putchar(z);
+	return (d);
+}
