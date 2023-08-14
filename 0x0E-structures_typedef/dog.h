a3dce7ecacbbc6efca88050b085da318f46be304
MayDan12 <oluwamayowadan@gmail.com>
2023-08-14 15:02:59 -0700

    dog.hu

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,6 +17,12 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - this is the typedef for struct dog
+ * daniel mayowa
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
