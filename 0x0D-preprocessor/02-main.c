34385b4e48497d9d83d5ebdb787477fed1f909d8
MayDan12 <oluwamayowadan@gmail.com>
2023-08-14 10:47:02 -0700

    love

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this print the name of the file
+ * written by daniel oluwanimotele
+ * Return: 0 always success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
