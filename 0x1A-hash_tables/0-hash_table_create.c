ea1514389107ef57dc90bae1452ca4d82dd12e02
MayDan12 <oluwamayowadan@gmail.com>
2023-11-02 14:37:45 -0700

    good

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,8 +9,8 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table = (hash_table_t *)malloc(sizeof(hash_table_t));
-	unsigned long int i = 0;
+	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
+	unsigned long int i;
 
 	if (hash_table == NULL)
 	{
@@ -19,11 +19,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	hash_table->size = size;
 
-	hash_table->array = (hash_node_t **)malloc(size * sizeof(hash_node_t *));
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (hash_table->array == NULL)
 	{
-		free(hash_table);
 		return (NULL);
 	}
 
